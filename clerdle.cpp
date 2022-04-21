9ab04946b07a6fd3111a133269fa11512eb869fa
hingobway <hingobway@users.noreply.github.com>
2022-04-20 23:06:09 -0400

    init

---
+++ clerdle.cpp
@@ -0,0 +1,21 @@
+// /////////////////////////////////////////////////////////
+//
+// File:   clerdle/clerdle.cpp
+// Author: Michael Foster
+// Date:   2022.04.29
+//
+// A command line port of the game Nerdle [nerdlegame.com].
+// Guess the random arithmetic equation in 6 guesses or
+// less.
+//
+// This is the program entrypoint.
+//
+// /////////////////////////////////////////////////////////
+
+int main()
+{
+
+  //
+
+  return 0;
+}
